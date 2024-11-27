43035d42140bedef1fc5e29914945347129291d1
duongCyberclone123 <duong.maibk106khmt@hcmut.edu.vn>
2024-11-27 19:44:05 +0700

    Update backend with sorting Printer

--- backend/src/controller/printingProcess.Controller.js
+++ backend/src/controller/printingProcess.Controller.js
@@ -15,6 +15,7 @@ class StudentController{
                 })
             }
             const response = await StudentService.uploadFile(studentID,req.body)
+            console.log(response)
             return res.status(200).json(response)
         }
         catch (err) {
