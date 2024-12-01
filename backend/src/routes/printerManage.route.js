5ef4ade57620a1a91f660974d6286f75bf1e4ab0
duongCyberclone123 <duong.maibk106khmt@hcmut.edu.vn>
2024-12-01 23:52:19 +0700

    Update Backend

--- backend/src/routes/printerManage.route.js
+++ backend/src/routes/printerManage.route.js
@@ -3,11 +3,14 @@ const PrinterController = require('../controller/printerManage.Controller')
 
 const router = express.Router();
 
+
+
+
 // Route thêm máy in
 
 router.get('/', PrinterController.getPrinter);
 router.post('/add', PrinterController.addPrinter);
 router.put('/update', PrinterController.updatePrinter);
 router.delete('/', PrinterController.deletePrinter);
-router.get('/viewLog', PrinterController.ViewHistoryLog)
+
 module.exports = router;
--- backend/src/routes/printingProcess.js
+++ backend/src/routes/printingProcess.js
@@ -3,7 +3,7 @@ const router = express.Router();
 //const { body, param } = require('express-validator');
 const studentController = require('../controller/printingProcess.Controller');
 const authMiddleware = require('../middleware/authMiddleware');
-
+const client = require('../../database/database')
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
@@ -22,7 +22,82 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage: storage });
+async function listAllPrintingLog(Log) {
+    let query = 'SELECT * FROM TRANSACTION'
+    let params = []
+    if (Log.sid) {
+        query += ' AND SID = ?'
+        params.push(Log.sid)
+    }
+    if (Log.sid && Log.pid) {
+        
+        query += ' AND PID = ?'
+        params.push(Log.pid)
+    }
+    else if (Log.pid){
+        query += ' WHERE PID = ?'
+        params.push(Log.pid)
+    }
 
+    if (Log.pid && Log.startTime){
+        
+        query += ' AND TSTART_TIME >= ?'
+        params.push(Log.startTime)
+    }
+    else if (Log.startTime){
+        query += ' WHERE TSTART_TIME >= ?'
+        params.push(Log.startTime)
+    }
+
+
+    if (Log.startTime && Log.endTime){
+        query += ' AND TEND_TIME <= ?'
+        params.push(Log.endTime)
+    }
+    else if (Log.endTime){
+        query += ' WHERE TEND_TIME <= ?'
+        params.push(Log.endTime)
+    }
+    return new Promise((resolve, reject) => {
+        client.query(
+            query, 
+            params,
+            (err, res) => {
+                if (err) {
+                    reject({
+                        status: 400,
+                        msg: err.message,
+                        data: null
+                    });
+                } else {
+                    resolve({
+                        status: 200,
+                        msg: 'Fetch success',
+                        data: res
+                    });
+                }
+            }
+        );
+    });
+} 
+
+async function ViewHistoryLog(req, res){
+    try{
+        const log = await listAllPrintingLog(req.body);
+        return res.status(200).json({
+            status: 200,
+            msg: "View Log",
+            data: log
+        })
+    }
+    catch(err){
+        return res.status(404).json({
+            status: 404,
+            msg: err,
+            data: null
+        })
+    }
+}
 router.post('/uploadFile',upload.single('file'), studentController.uploadFile);
 router.get('/getPrinters',studentController.PrintersInLocation)
 //router.post('/processPrinting/:studentID/:docID',studentController.Printing)
@@ -30,4 +105,5 @@ router.post('/receivePrintingRequest',studentController.receivePrintingRequest)
 router.post('/Printing',studentController.Printing)
 router.get('/viewHistoryLog',studentController.ViewHistoryLog)
 router.post('/buyPage', studentController.purchasePaper)
+router.get('/viewLog', ViewHistoryLog)
 module.exports = router
